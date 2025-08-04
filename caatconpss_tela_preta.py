129fec5eecc81305b4f74b42ab9fcdf89939615a
Tiago Sales <tiagoasales@gmail.com>
2025-08-04 13:32:12 -0300

    pss tela preta

--- caatconpss_tela_preta.py
+++ caatconpss_tela_preta.py
@@ -1,45 +1,12 @@
 import MacrosClasses
 import pandas as pd
 
-df = pd.read_csv('./input/02692716787.csv', dtype=str)
+import time
+
+df = pd.read_csv('./input/8821277720.csv', dtype=str)
 df = df.fillna(' ')
 
-cpf = df.iloc[0]['CPF']
-
 Acesso = MacrosClasses.janela3270("Terminal 3270 - A - AWVAKJR7")
-
-Acesso.Tab(1)
-Acesso.Digita(cpf)
-Tela = Acesso.ViraTelaSiafiTecla('ENTER')
-
-# DIGITA OS DADOS FICTICIOS PARA CAIR NO PSS
-Acesso.Digita("014003")
-Acesso.Digita("40")
-Acesso.Digita("02")
-Acesso.Tab(1)
-
-# NO DEFINITIVO MUDAR PARA 1998
-Acesso.Digita("01JAN1999") 
-Acesso.Digita("534")
-Acesso.Tab(1)
-
-Acesso.Digita("PROVIMENTO")
-Acesso.Tab(1)
-
-Acesso.Digita("01JAN2025")
-Acesso.Digita("534")
-Acesso.Tab(1)
-Acesso.Digita("VACANCIA")
-Acesso.Tab(1)
-
-Acesso.Digita("N")
-
-Acesso.ViraTelaSiafiTecla('ENTER')
-
-Acesso.Digita("x")
-Acesso.ViraTelaSiafiTecla('ENTER')
-Acesso.ViraTelaSiafiTecla('ENTER')
-Acesso.ViraTelaSiafiTecla('ENTER')
 
 # AQUI EH QUE REALMENTE COMEÇA O LANÇAMENTO DO PSS
 anos = df['ANO'].unique()
@@ -76,7 +43,7 @@ for ano in anos:
         else:
             pulos = mes - (mes_anterior + 1)
 
-        if pulos > 0:
+        if int(ano) != 1998 and pulos > 0 :
             Acesso.Tab(pulos)
 
         if mes in (1,2,3,4,5,6,7,8,9,10,11):
@@ -88,7 +55,7 @@ for ano in anos:
         mes_anterior =  int(row['MES'])
 
     Acesso.ViraTelaSiafiTecla('ENTER')
-    
+        
     # pop-up para assinalar a opção de informar o PSS
     Acesso.Tab(1)
     Acesso.Digita("X")
@@ -104,7 +71,8 @@ for ano in anos:
             Acesso.Digita('1')
             Acesso.Digita(row['VALOR'])
             Acesso.Tab(1)
-
-    break
+    
     Acesso.ViraTelaSiafiTecla('ENTER')
-
+    Acesso.Digita("C")
+    Acesso.ViraTelaSiafiTecla('ENTER')
+    Acesso.ViraTelaSiafiTecla('ENTER')
---
+++ caatconpss_tela_preta_pre.py
@@ -0,0 +1,44 @@
+import MacrosClasses
+
+# MUDAR O CPF AQUI!!!!!!
+cpf = '8821277720'
+
+# completa zero a esqueda
+cpf = cpf.zfill(11)
+
+Acesso = MacrosClasses.janela3270("Terminal 3270 - A - AWVAKJR7")
+
+Acesso.Tab(1)
+Acesso.Digita(cpf)
+Tela = Acesso.ViraTelaSiafiTecla('ENTER')
+
+# DIGITA OS DADOS FICTICIOS PARA CAIR NO PSS NO CAINPCAHIS
+Acesso.Digita("014003")
+Acesso.Digita("40")
+Acesso.Digita("02")
+Acesso.Tab(1)
+
+# NO DEFINITIVO MUDAR PARA 1998
+Acesso.Digita("01JAN1999") 
+Acesso.Digita("534")
+Acesso.Tab(1)
+
+Acesso.Digita("PROVIMENTO")
+Acesso.Tab(1)
+
+Acesso.Digita("01JAN2025")
+Acesso.Digita("534")
+Acesso.Tab(1)
+Acesso.Digita("VACANCIA")
+Acesso.Tab(1)
+
+Acesso.Digita("N")
+
+Acesso.ViraTelaSiafiTecla('ENTER')
+
+#Acesso.Tab(1)
+Acesso.Digita("x")
+Acesso.ViraTelaSiafiTecla('ENTER')
+Acesso.ViraTelaSiafiTecla('ENTER')
+Acesso.ViraTelaSiafiTecla('ENTER')
+
